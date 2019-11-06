031c76d8ec2920573e846ba324f1768e37fd1ecc
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-06 22:33:07 +0100

    Add clickable subpagfes 10.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,52 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
-  initMenu: function () {
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+      });
+    }
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      // if(page.id === pageId){
+      //   page.classList.add(classNames.pages.active);
+      // } else {
+      //   page.classList.remove(classNames.pages.active);
+      // }
+
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') === '#' + pageId);
+    }
+  },
+
+  initMenu: function() {
     const thisApp = this;
 
     for (let productData in thisApp.data.products) {
@@ -11,7 +54,7 @@ const app = {
     }
   },
 
-  initData: function () {
+  initData: function() {
     const thisApp = this;
 
     thisApp.data = {};
@@ -19,10 +62,10 @@ const app = {
     const url = settings.db.url + '/' + settings.db.product;
 
     fetch(url)
-      .then(function (rawResponse) {
+      .then(function(rawResponse) {
         return rawResponse.json();
       })
-      .then(function (parsedResponse) {
+      .then(function(parsedResponse) {
         console.log('parsedResponse', parsedResponse);
 
         /* save parsedResponse as thisApp.data.products */
@@ -35,7 +78,7 @@ const app = {
     console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
-  initCart: function () {
+  initCart: function() {
     const thisApp = this;
 
     const cartElem = document.querySelector(select.containerOf.cart);
@@ -43,13 +86,15 @@ const app = {
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
-    thisApp.productList.addEventListener('add-to-cart', function (event) {
+    thisApp.productList.addEventListener('add-to-cart', function(event) {
       app.cart.add(event.detail.product);
     });
   },
 
-  init: function () {
+  init: function() {
     const thisApp = this;
+
+    thisApp.initPages();
 
     thisApp.initData();
 
