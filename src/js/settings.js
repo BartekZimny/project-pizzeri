04501b8e9c4ef274a1c081569326b8b4401063cd
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-06 22:09:19 +0100

    Change files 10.2

--- src/js/settings.js
+++ src/js/settings.js
@@ -3,11 +3,14 @@
 export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
-    cartProduct: '#template-cart-product'
+    cartProduct: '#template-cart-product',
+    bookingWidget: '#template-booking-widget'
   },
   containerOf: {
     menu: '#product-list',
-    cart: '#cart'
+    cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper'
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -27,6 +30,15 @@ export const select = {
       input: 'input.amount',
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]'
+    },
+    datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`
+    },
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output'
     }
   },
   cart: {
@@ -46,6 +58,14 @@ export const select = {
     price: '.cart__product-price',
     edit: '[href="#edit"]',
     remove: '[href="#remove"]'
+  },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table'
+  },
+  nav: {
+    links: '.main-nav a'
   }
 };
 
@@ -56,26 +76,53 @@ export const classNames = {
   },
   cart: {
     wrapperActive: 'active'
+  },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked'
+  },
+  nav: {
+    active: 'active'
+  },
+  pages: {
+    active: 'active'
   }
 };
 
 export const settings = {
+  hours: {
+    open: 12,
+    close: 24
+  },
   amountWidget: {
     defaultValue: 1,
     defaultMin: 1,
     defaultMax: 9
   },
+  datePicker: {
+    maxDaysInFuture: 14
+  },
   cart: {
     defaultDeliveryFee: 20
+  },
+  booking: {
+    tableIdAttribute: 'data-table'
   },
   db: {
     url: '//localhost:3131',
     product: 'product',
-    order: 'order'
+    order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false'
   }
 };
 
 export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-  cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML)
+  cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML)
 };
