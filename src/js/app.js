3d1f412b97b7e71ca0327baaba53febb23f371f2
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-06 23:04:42 +0100

    Add class and file Booking 10.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function() {
---
+++ src/js/components/Booking.js
@@ -0,0 +1,15 @@
+class Booking {
+    constructor() {
+      const thisBooking = this;
+    }
+  
+    render() {
+      const thisBooking = this;
+    }
+  
+    initWidgets() {
+      const thisBooking = this;
+    }
+  }
+  
+  export default Booking;
