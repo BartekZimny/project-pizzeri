4e1da2a2b7a2eb697fb7634c37e2e5e4d89175bb
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-07 22:09:31 +0100

    Add class inheritance 10.3

---
+++ src/js/components/BaseWidget.js
@@ -0,0 +1,50 @@
+class BaseWidget {
+  constructor(wrapperElement, initialValue) {
+    const thisWidget = this;
+
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapperElement;
+
+    thisWidget.value = initialValue;
+  }
+
+  setValue(value) {
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(value);
+
+    /* TODO: Add validation */
+
+    if (newValue != thisWidget.value && thisWidget.isValid(newValue)) {
+      thisWidget.value = newValue;
+      thisWidget.announce();
+    }
+
+    thisWidget.renderValue();
+  }
+
+  parseValue(value) {
+    return parseInt(value);
+  }
+
+  isValid(value) {
+    return !isNaN(value);
+  }
+
+  renderValue() {
+    const thisWidget = this;
+
+    thisWidget.dom.wrapper.innerHTML = thisWidget.value;
+  }
+
+  announce() {
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.dom.wrapper.dispatchEvent(event);
+  }
+}
+
+export default BaseWidget;
