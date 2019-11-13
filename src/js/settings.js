9d7df8cfc6e1674b391404806c78f86ff59bced9
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-11-14 00:05:05 +0100

    Fix Booking error 11.2

--- src/js/settings.js
+++ src/js/settings.js
@@ -63,7 +63,12 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
-    tablesReserved: '.floor-plan .table.reservation'
+    tablesReserved: '.floor-plan .table.reservation',
+    form: '.booking-form',
+    formSubmit: '.booking-form [type="submit"]',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+    starters: '.checkbox [type="checkbox"]'
   },
   nav: {
     links: '.main-nav a'
